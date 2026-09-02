13e68ef3dfa32f5e70630904d59f7b20d659726c
Anas Naseem <anasalansari06@example.com>
2026-09-02 22:30:03 +0530

    Add API tests

--- mechanics/tests.py
+++ mechanics/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
 
-# Create your tests here.
+from .models import Mechanic
+
+
+class MechanicAPITest(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+
+        Mechanic.objects.create(
+            name="Test Mechanic",
+            phone="9876543210",
+            location="Noida",
+            rating=4.5,
+            is_open=True,
+            services="Oil Change"
+        )
+
+    def test_get_mechanics(self):
+        response = self.client.get('/api/mechanics/')
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_mechanic_by_id(self):
+        response = self.client.get('/api/mechanics/1/')
+
+        self.assertEqual(response.status_code, 200)
